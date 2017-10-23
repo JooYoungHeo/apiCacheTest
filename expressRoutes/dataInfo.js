28018c53978576994a1e5d56f97867a8968da346
JooYoungHeo <jyhur900226@naver.com>
2017-10-23 09:49:35 +0900

    [Work] check elapsed time

--- expressRoutes/dataInfo.js
+++ expressRoutes/dataInfo.js
@@ -8,8 +8,11 @@ let cache = apicache.middleware
 let {Sports} = require(path.join(process.cwd(), 'models'));
 
 router.get('/', cache('1 minutes'), (req, res) => {
+  let start = new Date().getTime();
   findSports().then(result => {
     res.json(result);
+    let end = new Date().getTime();
+    console.log(`${end - start}ms`);
   }).catch(err => {
     res.status(500).json(err);
   })
